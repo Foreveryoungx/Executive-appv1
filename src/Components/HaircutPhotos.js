ac8ea0669c8d91ec0d5f8ef078f2093cabd6abd7
Trey Allen <trevon.allen1994@gmail.com>
2021-12-23 01:49:49 -0600

    Update color Scheme

--- src/Components/HaircutPhotos.js
+++ src/Components/HaircutPhotos.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ImageGallery from "react-image-gallery";
 import img1 from "../Images/img1.jpg";
 import img2 from "../Images/img2.jpeg";
-import img8 from "../Images/img8.jpg";
 import img9 from "../Images/img9.jpg";
 import img6 from "../Images/img6.jpg";
 const images =[
